93d1224b30db5e716bf4210ea912a2dfb00b4a21
sharf <373455742@qq.com>
2016-07-30 00:01:31 +0800

    init

--- app/Controller/WxapiController.php
+++ app/Controller/WxapiController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/7/29
- * Time: 23:16
- */
-
 namespace App\Controller;
 
 use EasyWeChat\Foundation\Application;
@@ -17,22 +10,6 @@ class WxapiController extends Controller
     public function __construct()
     {
         parent::__construct();
-//        $options = [
-//            'debug' => true,
-//            'app_id' => 'wx2dc7b9baa7afd65b',
-//            'secret' => '56d196f91373e6c3acadba655f2ba5cd',
-//            'token' => 'print',
-//            // 'aes_key' => null, // 可选
-//            'log' => [
-//                'level' => 'debug',
-//                'file' => ROOT.'/public/easywechat.log', // XXX: 绝对路径！！！！
-//            ]
-//        ];
-//        $app=new Application($options);
-//        $this->server = $app->server;
-    }
-    public function index()
-    {
         $options = [
             'debug' => true,
             'app_id' => 'wx2dc7b9baa7afd65b',
@@ -44,11 +21,11 @@ class WxapiController extends Controller
                 'file' => ROOT.'/public/easywechat.log', // XXX: 绝对路径！！！！
             ]
         ];
-        $app=new \EasyWeChat\Foundation\Application($options);
-        $app->server->serve()->send();
+        $app=new Application($options);
+        $this->server = $app->server;
     }
 
-    public function index1()
+    public function index()
     {
         $this->server->setMessageHandler(function ($message) {
             //return "您好！欢迎关注我!";
