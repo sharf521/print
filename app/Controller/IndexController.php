836804bfe2a6e14ef025f66b0edd2a27e6c144c2
sharf <373455742@qq.com>
2016-07-29 21:36:16 +0800

    init

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -13,6 +13,24 @@ class IndexController extends Controller
 
     public function index()
     {
-        
+        $options = [
+            'debug' => true,
+            'app_id' => 'wx2dc7b9baa7afd65b',
+            'secret' => '56d196f91373e6c3acadba655f2ba5cd',
+            'token' => 'print',
+            // 'aes_key' => null, // 可选
+            'log' => [
+                'level' => 'debug',
+                'file' => ROOT.'/public/data/easywechat.log', // XXX: 绝对路径！！！！
+            ]
+        ];
+
+// 使用配置来初始化一个项目。
+        $app=new \EasyWeChat\Foundation\Application($options);
+        $response = $app->server->serve();
+// 将响应输出
+        $response->send(); // Laravel 里请使用：return $response;
+
+        exit;
     }
 }
