28bd50883a359fdc01dd857c0df010c3dcd780f1
sharf <373455742@qq.com>
2016-07-29 23:44:17 +0800

    init

--- app/Controller/WxapiController.php
+++ app/Controller/WxapiController.php
@@ -31,43 +31,13 @@ class WxapiController extends Controller
         $app=new Application($options);
         $this->server = $app->server;
     }
-    public function index()
-    {
-        ob_start();
-        $options = [
-            'debug' => true,
-            'app_id' => 'wx2dc7b9baa7afd65b',
-            'secret' => '56d196f91373e6c3acadba655f2ba5cd',
-            'token' => 'print',
-            // 'aes_key' => null, // 可选
-            'log' => [
-                'level' => 'debug',
-                'file' => ROOT.'/public/easywechat.log', // XXX: 绝对路径！！！！
-            ]
-        ];
-        $app=new \EasyWeChat\Foundation\Application($options);
-        $app->server->serve()->send();
-
-        $phpinfo = ob_get_contents();
-        file_put_contents("log1.txt",$phpinfo);
-        ob_clean();
-        echo $phpinfo;
-        exit;
-    }
 
     public function index1()
     {
-        ob_start();
         $this->server->setMessageHandler(function ($message) {
             //return "您好！欢迎关注我!";
             return new Text(['content' => '您好！overtrue。']);
         });
         $this->server->serve()->send();
-        $phpinfo = ob_get_contents();
-        file_put_contents("log1.txt",$phpinfo);
-        ob_clean();
-        echo $phpinfo;
-
-        exit;
     }
 }
