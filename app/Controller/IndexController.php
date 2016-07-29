dee8e0946c14444f87b23b3420d4ef3e61800a95
sharf <373455742@qq.com>
2016-07-29 21:34:30 +0800

    init

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -13,25 +13,6 @@ class IndexController extends Controller
 
     public function index()
     {
-        $options = [
-            'debug' => true,
-            'app_id' => app('\App\Model\System')->getCode('appid'),
-            'secret' => app('\App\Model\System')->getCode('appsecret'),
-            'token' => 'print',
-            // 'aes_key' => null, // 可选
-            'log' => [
-                'level' => 'debug',
-                'file' => ROOT.'/public/data/easywechat.log', // XXX: 绝对路径！！！！
-            ],
-            //...
-        ];
-
-        print_r($options);
-
-        // 使用配置来初始化一个项目。
-        $app=new Application($options);
-        $response = $app->server->serve();
-        // 将响应输出
-        $response->send(); // Laravel 里请使用：return $response;
+        
     }
 }
--- index.php
+++ index.php
@@ -18,6 +18,28 @@ set_time_limit($set_time = 3600);
 define('ROOT', __DIR__);
 $_G = array();
 require ROOT . '/vendor/autoload.php';
+
+
+$options = [
+    'debug' => true,
+    'app_id' => 'wx2dc7b9baa7afd65b',
+    'secret' => '56d196f91373e6c3acadba655f2ba5cd',
+    'token' => 'print',
+    // 'aes_key' => null, // 可选
+    'log' => [
+        'level' => 'debug',
+        'file' => ROOT.'/public/data/easywechat.log', // XXX: 绝对路径！！！！
+    ]
+    //...
+];
+
+// 使用配置来初始化一个项目。
+$app=new \EasyWeChat\Foundation\Application($options);
+$response = $app->server->serve();
+// 将响应输出
+$response->send(); // Laravel 里请使用：return $response;
+
+exit;
 require ROOT . '/system/Autoloader.php';
 use System\Lib\DB;
 DB::instance('db1');
