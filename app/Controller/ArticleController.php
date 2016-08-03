04e4359c8bfd232bf7dc51e7e5f851745ebcda19
sharf <373455742@qq.com>
2016-08-03 19:20:42 +0800

    init

---
+++ app/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use App\Model\Article;
+use System\Lib\Request;
+
+class ArticleController extends Controller
+{
+    public  function  index()
+    {
+
+    }
+
+    public function detail(Request $request,Article $article)
+    {
+        $id=$request->get(2);
+        $row=$article->findOrFail($id);
+        $row->content=$row->ArticleData()->content;
+        $data['article']=$row;
+        $this->view('article',$data);
+    }
+}
