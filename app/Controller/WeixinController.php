f433cec39142dcee02ef44fa2e4a5bdc245cadac
sharf521 <373455742@qq.com>
2016-08-07 20:46:39 +0800

    add admin order

--- app/Controller/WeixinController.php
+++ app/Controller/WeixinController.php
@@ -80,11 +80,21 @@ class WeixinController extends Controller
         }
 
         if($_POST){
+
+
+
+
+
+        }else{
+            $data['task']=$task;
+            $data['order']=$task->PrintOrder();
+            $data['title_herder']='我的订单';
+
+
             $openid=DB::table('user')->where('id=?')->bindValues($this->user_id)->value('openid');
             $weChat=new WeChat();
             $app=$weChat->app;
             $payment = $app->payment;
-
             $attributes = [
                 'trade_type'       => 'JSAPI', // JSAPI，NATIVE，APP...
                 'body'             => 'iPad mini 16G 白色',
@@ -94,45 +104,15 @@ class WeixinController extends Controller
                 'attach'=>'attach',
                 'product_id'=>1,
                 'openid'=>$openid,
-                'notify_url'       => 'http://print.yuantuwang.com/weixin/order-notify', // 支付结果通知网址，如果不设置则会
+                'notify_url'       => 'http://print.yuantuwang.com/weixin/order-notify'
             ];
-
             $order=new Order($attributes);
             $result = $payment->prepare($order);
-
-            $result = $payment->prepare($order);
             if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
-                $prepayId = $result->prepay_id;
-
                 $js = $app->js;
-                ?>
-                <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
-                <script type="text/javascript" charset="utf-8">
-                    wx.config(<?php echo $js->config(array('chooseWXPay'), true) ?>);
-                    wx.chooseWXPay({
-                        timestamp: <?=time()?>, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                        nonceStr: '<?=$result->sign?>', // 支付签名随机串，不长于 32 位
-                        package: 'prepay_id=<?=$result->prepay_id?>', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                        signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                        paySign: '<?=$result->sign?>', // 支付签名
-                        success: function (res) {
-                            // 支付成功后的回调函数
-                        }
-                    });
-                </script>
-
-                <?
-
-
+                $data['config']=$js->config(array('chooseWXPay'), true);
+                $data['pay']=$result;
             }
-
-
-
-
-        }else{
-            $data['task']=$task;
-            $data['order']=$task->PrintOrder();
-            $data['title_herder']='我的订单';
             $this->view('print',$data);
         }
     }
