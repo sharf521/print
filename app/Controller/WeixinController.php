94b1825ca11c707f955bb3ae103b532dafacade1
sharf <373455742@qq.com>
2016-08-08 13:22:33 +0800

    init

--- app/Controller/WeixinController.php
+++ app/Controller/WeixinController.php
@@ -111,7 +111,14 @@ class WeixinController extends Controller
             if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
                 $js = $app->js;
                 $data['config']=$js->config(array('chooseWXPay'), true);
-                $data['pay']=$result;
+                $pay['timestamp']=time();
+                $pay['nonceStr']=$this->getNonceStr();
+                $pay['package']="prepay_id={$result->prepay_id}";
+                $pay['signType']='MD5';
+                $pay['paySign']=$this->MakeSign($pay);
+
+                print_r($pay);
+                $data['pay']=$pay;
             }
             $this->view('print',$data);
         }
@@ -121,4 +128,56 @@ class WeixinController extends Controller
     {
         echo '联盟页';
     }
+
+
+
+    /**
+     *
+     * 产生随机字符串，不长于32位
+     * @param int $length
+     * @return 产生的随机字符串
+     */
+    public static function getNonceStr($length = 32)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str ="";
+        for ( $i = 0; $i < $length; $i++ )  {
+            $str .= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+        }
+        return $str;
+    }
+    /**
+     * 格式化参数格式化成url参数
+     */
+    public function ToUrlParams($values)
+    {
+        $buff = "";
+        foreach ($values as $k => $v)
+        {
+            if($k != "sign" && $v != "" && !is_array($v)){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    /**
+     * 生成签名
+     */
+    public function MakeSign($values)
+    {
+        //签名步骤一：按字典序排序参数
+        ksort($values);
+        $string = $this->ToUrlParams($values);
+        //签名步骤二：在string后加入KEY
+        $string = $string . "&key=".'kfjakdfjakldsfjkasdq1234123411as';
+        //签名步骤三：MD5加密
+        $string = md5($string);
+        //签名步骤四：所有字符转为大写
+        $result = strtoupper($string);
+        return $result;
+    }
+
 }
--- app/themes/weixin/print.tpl.php
+++ app/themes/weixin/print.tpl.php
@@ -107,11 +107,11 @@
         wx.config(<?=$config?>);
         wx.ready(function(){
             wx.chooseWXPay({
-                timestamp: <?=time()?>, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                nonceStr: '<?=$pay->nonce_str?>', // 支付签名随机串，不长于 32 位
-                package: 'prepay_id=<?=$pay->prepay_id?>', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                timestamp: <?=$pay['timestamp']?>, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                nonceStr: '<?=$pay['nonceStr']?>', // 支付签名随机串，不长于 32 位
+                package: '<?=$pay['package']?>', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                 signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                paySign: '<?=$pay->sign?>', // 支付签名
+                paySign: '<?=$pay['paySign']?>', // 支付签名
                 success: function (res) {
                     // 支付成功后的回调函数
                 }
