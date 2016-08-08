26c969b1fd26ba97a038e2e68a3bd98b88ac743e
sharf521 <373455742@qq.com>
2016-08-08 22:55:14 +0800

    add admin order

--- app/Controller/Admin/ArticleController.php
+++ app/Controller/Admin/ArticleController.php
@@ -147,7 +147,6 @@ class ArticleController extends AdminController
         } else {
             //一级分类
             $data['cates'] = $category->getlist(array('pid' => 1));
-            var_dump($data['cates']);
             $data['row'] = $article->findOrFail($id);
             $data['row']->content = $data['row']->ArticleData()->content;
             $categorypath = explode(',', $data['row']->category_path);
