d4d5ea346f3e24fbd26a8be6100d5b96622f8174
sharf521 <373455742@qq.com>
2016-08-25 22:07:58 +0800

    add shop

--- app/themes/weixin/shop.tpl.php
+++ app/themes/weixin/shop.tpl.php
@@ -1,7 +1,7 @@
 <?php require 'header.php';?>
 <script src="/plugin/js/ajaxfileupload.js?111"></script>
 <? if($this->func=='index') : ?>
-    <div class="header_tit">店铺列表<a class="header_right" href="<?=url('shop/add')?>">添加</a></div>
+    <div class="header_tit">我的店铺<a class="header_right" href="<?=url('shop/add')?>">添加</a></div>
     <div class="shop_list">
         <? if(empty($list)) : ?>
             <div class='alert-warning'>没有找到匹配的记录！</div>
