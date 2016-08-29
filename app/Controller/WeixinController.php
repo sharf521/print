05d6859da02e8af6b87f1042843509b328e174ca
sharf <373455742@qq.com>
2016-08-29 17:55:10 +0800

    address

--- app/Controller/WeixinController.php
+++ app/Controller/WeixinController.php
@@ -137,7 +137,7 @@ class WeixinController extends Controller
     public function invite(WeChat $weChat,User $user)
     {
         $data['qrcodeSrc']=$weChat->qrcode($this->user_id.'01');
-        $data['invites']=$user->where("invite_userid=?")->bindValues($this->user_id)->get();
+        $data['invites']=$user->where("invite_userid=?")->bindValues($this->user_id)->orderBy('id desc')->get();
         $data['title_herder']='邀请商家';
         $this->view('invite',$data);
     }
