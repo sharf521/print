8ba4068c7aa56a4356f2f8e31b6308d4a1616b9b
sharf521 <373455742@qq.com>
2016-10-30 19:56:34 +0800

    同步用户

--- app/Controller/Admin/UserController.php
+++ app/Controller/Admin/UserController.php
@@ -63,6 +63,16 @@ class UserController extends AdminController
             }
         }
         echo '完成';
+
+        /*** 删除所有的session变量..也可用unset($_SESSION[xxx])逐个删除。****/
+        $_SESSION = array();
+        /***删除sessin id.由于session默认是基于cookie的，所以使用setcookie删除包含session id的cookie.***/
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time()-42000, '/');
+        }
+        // 最后彻底销毁session.
+        session_destroy();
+
     }
 
     function add()
