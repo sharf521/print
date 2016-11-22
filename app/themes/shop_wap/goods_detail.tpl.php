e8ac41876700af50105ed0e5e46b679f3e2408f1
sharf <373455742@qq.com>
2016-11-22 16:51:06 +0800

    html

--- app/themes/shop_wap/goods_detail.tpl.php
+++ app/themes/shop_wap/goods_detail.tpl.php
@@ -1,14 +1,12 @@
 <?php require 'header.php';?>
-    <!--googpic-->
     <div class="swiper-container">
-        <div class="swiper-wrapper" style="height: 320px;">
+        <div class="swiper-wrapper" style="height: 300px;">
             <? foreach($images as $img) : ?>
-            <div class="swiper-slide" style="text-align: center"><a href="#"><img src="<?=$img->image_url?>" style="max-width: 100%; max-height: 100%"></a></div>
+                <div class="swiper-slide" style="text-align: center"><a href="#"><img src="<?=$img->image_url?>" style="max-width: 100%; max-height: 100%"></a></div>
             <? endforeach;?>
         </div>
         <div class="swiper-pagination"></div>
     </div>
-    <!--googpic end-->
     <div class="goods-header">
         <h2 class="title"><?=$goods->name?></h2>
         <div class="goods-price">
@@ -18,24 +16,23 @@
             运费: ¥<?=$goods->shipping_fee?> &nbsp;　&nbsp;剩余:<?=$goods->stock_count?>
         </div>
     </div>
+
     <div class="weui-panel" style="margin-bottom: 60px;">
         <div class="weui-panel__hd">详细说明</div>
-        <div class="weui-panel__bd">
-            <div class="weui-media-box weui-media-box_text">
-                <p class="weui-media-box__desc"><?=nl2br($GoodsData->content)?></p>
-            </div>
-        </div>
+        <article class="weui-article">
+            <?=nl2br($GoodsData->content)?>
+        </article>
     </div>
 
-<div class="bottom_opts">
-    <a href="javascript:;" class="opt1">
-        <i class="iconfont">&#xe698;</i>
-        <p>购物车</p>
-    </a>
-    <a href="javascript:;" class="opt2">加入购物车</a>
+    <div class="bottom_opts">
+        <a href="javascript:;" class="opt1">
+            <i class="iconfont">&#xe698;</i>
+            <p>购物车</p>
+        </a>
+        <a href="javascript:;" class="opt2">加入购物车</a>
 
-    <a href="javascript:;" class="opt3">立即购买</a>
-</div>
+        <a href="javascript:;" class="opt3">立即购买</a>
+    </div>
     <script>
         //首页幻灯图
         $(function(){
