3b42d55ae2a8bdd86f886312bedd5fc8056dabd2
sharf <373455742@qq.com>
2016-12-02 12:23:21 +0800

    goods

--- app/Controller/WxController.php
+++ app/Controller/WxController.php
@@ -14,6 +14,19 @@ class WxController extends Controller
     public function __construct()
     {
         parent::__construct();
+        $msg=json_encode($_REQUEST);
+        $file_path = ROOT . "/public/data/wx/";
+        if (!is_dir($file_path)) {
+            mkdir($file_path, 0777, true);
+        }
+        $filename = $file_path . date("Ym") . "_index.log";
+        $fp = fopen($filename, "a+");
+        $time = date('Y-m-d H:i:s');
+        $ip=$this->ip();
+        $file = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER["REQUEST_URI"];
+        $str = "time:{$time}\t ip:{$ip}}\t{error:" . $msg . "}\t file:{$file}\t\r\n";
+        fputs($fp, $str);
+        fclose($fp);
     }
 
     public function index()
