28b486a1adf50f0c6c3e513ab231fb5b428cea7f
sharf <373455742@qq.com>
2016-12-02 12:44:09 +0800

    goods

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -7,4 +7,20 @@ class IndexController extends Controller
     {
         parent::__construct();
     }
+
+    public function index()
+    {
+        $msg=json_encode($_REQUEST);
+        $file_path = ROOT . "/public/data/wx/";
+        if (!is_dir($file_path)) {
+            mkdir($file_path, 0777, true);
+        }
+        $filename = $file_path . date("Ym") . "_index_index11.log";
+        $fp = fopen($filename, "a+");
+        $time = date('Y-m-d H:i:s');
+        $file = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER["REQUEST_URI"];
+        $str = "time:{$time} \t{error:" . $msg . "}\t file:{$file}\t\r\n";
+        fputs($fp, $str);
+        fclose($fp);
+    }
 }
